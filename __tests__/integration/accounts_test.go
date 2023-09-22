0e0a1d81d607c8297177fda59548b83225861ea9
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-22 14:20:35 -0500

    test(session): Add tests for the new endpoint

--- __tests__/integration/accounts_test.go
+++ __tests__/integration/accounts_test.go
@@ -4,8 +4,14 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/UPB-Code-Labs/main-api/src/accounts/infrastructure/requests"
 	"github.com/stretchr/testify/require"
 )
+
+type RegisterTestCase struct {
+	Payload            map[string]interface{}
+	ExpectedStatusCode int
+}
 
 func TestRegisterStudent(t *testing.T) {
 	c := require.New(t)
@@ -13,7 +19,7 @@ func TestRegisterStudent(t *testing.T) {
 	// Register route
 	router.POST("/accounts/students", accountsControllers.HandleRegisterStudent)
 
-	testCases := []TestCase{
+	testCases := []RegisterTestCase{
 		{
 			Payload: map[string]interface{}{
 				"full_name":        "John Doe",
@@ -55,10 +61,22 @@ func TestRegisterStudent(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		w, r := PerformRequest("POST", "/accounts/students", testCase.Payload)
+		w, r := PrepareRequest("POST", "/accounts/students", testCase.Payload)
 		router.ServeHTTP(w, r)
 		c.Equal(testCase.ExpectedStatusCode, w.Code)
 	}
+}
+
+func RegisterStudent(req requests.RegisterUserRequest) int {
+	w, r := PrepareRequest("POST", "/accounts/students", map[string]interface{}{
+		"full_name":        req.FullName,
+		"email":            req.Email,
+		"institutional_id": req.InstitutionalId,
+		"password":         req.Password,
+	})
+
+	router.ServeHTTP(w, r)
+	return w.Code
 }
 
 func TestRegisterAdmin(t *testing.T) {
@@ -67,7 +85,7 @@ func TestRegisterAdmin(t *testing.T) {
 	// Register route
 	router.POST("/accounts/admins", accountsControllers.HandleRegisterAdmin)
 
-	testCases := []TestCase{
+	testCases := []RegisterTestCase{
 		{
 			Payload: map[string]interface{}{
 				"full_name": "John Doe",
@@ -95,8 +113,19 @@ func TestRegisterAdmin(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		w, r := PerformRequest("POST", "/accounts/admins", testCase.Payload)
+		w, r := PrepareRequest("POST", "/accounts/admins", testCase.Payload)
 		router.ServeHTTP(w, r)
 		c.Equal(testCase.ExpectedStatusCode, w.Code)
 	}
+}
+
+func RegisterAdmin(req requests.RegisterAdminRequest) int {
+	w, r := PrepareRequest("POST", "/accounts/admins", map[string]interface{}{
+		"full_name": req.FullName,
+		"email":     req.Email,
+		"password":  req.Password,
+	})
+
+	router.ServeHTTP(w, r)
+	return w.Code
 }
