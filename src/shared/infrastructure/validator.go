37cd6d219422062aa130afadb7bd50af6a5bf75f
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-22 11:34:43 -0500

    feat(accounts): Check students email is institutional

--- src/shared/infrastructure/validator.go
+++ src/shared/infrastructure/validator.go
@@ -1,12 +1,21 @@
 package infrastructure
 
-import "github.com/go-playground/validator/v10"
+import (
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var validate *validator.Validate
 
 func GetValidator() *validator.Validate {
 	if validate == nil {
 		validate = validator.New()
+
+		validate.RegisterValidation("institutional_email", func(fl validator.FieldLevel) bool {
+			email := fl.Field().String()
+			return strings.HasSuffix(email, "@upb.edu.co")
+		})
 	}
 
 	return validate
