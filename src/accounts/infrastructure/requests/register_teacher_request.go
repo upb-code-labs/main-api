7e8fd535574e16ee25a5619025e81a1907f0e721
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-24 20:25:49 -0500

    feat(accounts): Validate teacher's email is institutional

--- src/accounts/infrastructure/requests/register_teacher_request.go
+++ src/accounts/infrastructure/requests/register_teacher_request.go
@@ -4,7 +4,7 @@ import "github.com/UPB-Code-Labs/main-api/src/accounts/domain/dtos"
 
 type RegisterTeacherRequest struct {
 	FullName string `json:"full_name" validate:"required,min=4,max=255"`
-	Email    string `json:"email" validate:"required,email"`
+	Email    string `json:"email" validate:"required,email,institutional_email"`
 	Password string `json:"password" validate:"required,min=8,max=255,secure_password"`
 }
 
