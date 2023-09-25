b12373e26b95dfa5c265ebb42075485845044d46
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-09-24 19:00:22 -0500

    chore(session): Rename session cookie

--- src/session/infrastructure/http_controllers.go
+++ src/session/infrastructure/http_controllers.go
@@ -39,7 +39,7 @@ func (controllers *SessionControllers) HandleLogin(c *gin.Context) {
 	}
 
 	// Set the cookie
-	cookieName := "token"
+	cookieName := "session"
 	cookieSecondsTTL := infrastructure.GetEnvironment().JwtExpirationHours * 60 * 60
 	cookieDomain := ""
 	cookiePath := "/"
