a2cfb46b74ff7a39a096df1977c20444e13f6a1f
Pedro Andrés Chaparro Quintero <62714297+PedroChaparro@users.noreply.github.com>
2023-11-15 09:57:14 -0500

    fix: Min criteria weight (#91)

--- src/rubrics/infrastructure/requests/add_criteria_to_objective_request.go
+++ src/rubrics/infrastructure/requests/add_criteria_to_objective_request.go
@@ -2,5 +2,5 @@ package requests
 
 type AddCriteriaToObjectiveRequest struct {
 	Description string  `json:"description" validate:"required,min=8,max=510"`
-	Weight      float64 `json:"weight" validate:"required,numeric,min=0,max=100"`
+	Weight      float64 `json:"weight" default:"0" validate:"numeric,min=0,max=100"`
 }
