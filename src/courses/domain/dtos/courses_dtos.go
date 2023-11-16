ab881db969e3cd831784eab72fc469c2c21d4b5a
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-11-16 09:02:02 -0500

    refactor(courses): Group courses DTOs

---
+++ src/courses/domain/dtos/courses_dtos.go
@@ -0,0 +1,44 @@
+package dtos
+
+import "github.com/UPB-Code-Labs/main-api/src/courses/domain/entities"
+
+type AddStudentToCourseDTO struct {
+	TeacherUUID string
+	StudentUUID string
+	CourseUUID  string
+}
+
+type CreateCourseDTO struct {
+	Name        string
+	TeacherUUID string
+	Color       entities.Color
+}
+
+type EnrolledCoursesDto struct {
+	Courses       []entities.Course
+	HiddenCourses []entities.Course
+}
+
+type EnrolledStudentDTO struct {
+	UUID            string
+	FullName        string
+	Email           string
+	InstitutionalId string
+	IsActive        bool
+}
+
+type GetInvitationCodeDTO struct {
+	CourseUUID  string
+	TeacherUUID string
+}
+
+type JoinCourseUsingInvitationCodeDTO struct {
+	StudentUUID    string
+	InvitationCode string
+}
+
+type RenameCourseDTO struct {
+	TeacherUUID string
+	CourseUUID  string
+	NewName     string
+}
