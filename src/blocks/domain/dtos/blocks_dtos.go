412af39e7d3bd5c4752728c36a5ec321e68cd28c
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-12-29 20:36:09 -0500

    feat(blocks): Add endpoint to rename test blocks

--- src/blocks/domain/dtos/blocks_dtos.go
+++ src/blocks/domain/dtos/blocks_dtos.go
@@ -1,7 +1,17 @@
 package dtos
 
+import "mime/multipart"
+
 type UpdateMarkdownBlockContentDTO struct {
 	TeacherUUID string
 	BlockUUID   string
 	Content     string
+}
+
+type UpdateTestBlockDTO struct {
+	TeacherUUID    string
+	BlockUUID      string
+	LanguageUUID   string
+	Name           string
+	NewTestArchive *multipart.File
 }
--- src/blocks/infrastructure/requests/blocks_requests.go
+++ src/blocks/infrastructure/requests/blocks_requests.go
@@ -3,3 +3,8 @@ package requests
 type UpdateMarkdownBlockContentRequest struct {
 	Content string `json:"content" validate:"required"`
 }
+
+type UpdateTestBlockRequest struct {
+	LanguageUUID string `validate:"required,uuid4"`
+	Name         string `validate:"required,min=4,max=255"`
+}
