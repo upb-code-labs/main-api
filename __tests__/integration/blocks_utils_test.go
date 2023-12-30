fd522ed3bdd42d303d8041f9d9684eabb7f58955
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-12-29 20:13:10 -0500

    fix(tests): Fix broken tests due to endpoint verb change
    The endpoint verb was changed from PUT to PATCH

--- __tests__/integration/blocks_utils_test.go
+++ __tests__/integration/blocks_utils_test.go
@@ -7,7 +7,7 @@ import (
 
 func UpdateMarkdownBlockContent(cookie *http.Cookie, blockUUID string, payload map[string]interface{}) (response map[string]interface{}, statusCode int) {
 	endpoint := fmt.Sprintf("/api/v1/blocks/markdown_blocks/%s/content", blockUUID)
-	w, r := PrepareRequest("PUT", endpoint, payload)
+	w, r := PrepareRequest("PATCH", endpoint, payload)
 	r.AddCookie(cookie)
 	router.ServeHTTP(w, r)
 
