9e6ab9428172b27a6b7d1a5849aa35ed1b2daf51
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2024-01-14 20:16:55 -0500

    test(accounts): Add tests to ensure users can get and update their profile

--- __tests__/integration/accounts_utils_test.go
+++ __tests__/integration/accounts_utils_test.go
@@ -81,3 +81,34 @@ func UpdatePasswordUtil(dto UpdatePasswordUtilDTO) int {
 	router.ServeHTTP(w, r)
 	return w.Code
 }
+
+type UpdateProfileUtilDTO struct {
+	FullName        string
+	Email           string
+	Password        string
+	InstitutionalId *string
+	Cookie          *http.Cookie
+}
+
+// UpdateProfileUtil sends a request to update the profile of an account and returns the status code
+// of the response
+func UpdateProfileUtil(dto UpdateProfileUtilDTO) int {
+	w, r := PrepareRequest("PUT", "/api/v1/accounts/profile", map[string]interface{}{
+		"full_name":        dto.FullName,
+		"email":            dto.Email,
+		"password":         dto.Password,
+		"institutional_id": dto.InstitutionalId,
+	})
+	r.AddCookie(dto.Cookie)
+	router.ServeHTTP(w, r)
+	return w.Code
+}
+
+// GetProfileUtil sends a request to get the profile of an account and returns the JSON response
+// as a map and the status code of the response
+func GetProfileUtil(cookie *http.Cookie) (response map[string]interface{}, statusCode int) {
+	w, r := PrepareRequest("GET", "/api/v1/accounts/profile", nil)
+	r.AddCookie(cookie)
+	router.ServeHTTP(w, r)
+	return ParseJsonResponse(w.Body), w.Code
+}
