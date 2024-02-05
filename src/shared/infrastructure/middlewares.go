51bc73ea4cd9769478d4604d645f2e1914eaae4f
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2024-02-05 16:22:49 -0500

    fix(submissions): Add buffering header to `SSE` endpoint

--- src/shared/infrastructure/middlewares.go
+++ src/shared/infrastructure/middlewares.go
@@ -81,6 +81,7 @@ func WithServerSentEventsMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Cache-Control", "no-cache")
 		c.Writer.Header().Set("Connection", "keep-alive")
 		c.Writer.Header().Set("Transfer-Encoding", "chunked")
+		c.Writer.Header().Set("X-Accel-Buffering", "no")
 
 		c.Next()
 	}
